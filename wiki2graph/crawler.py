4e651ad656a572bb274e7b4724b1edbfc30fbde1
Ben <doc.ben.johnson@gmail.com>
2018-11-23 16:02:33 -0500

    iteration and limits

--- wiki2graph/crawler.py
+++ wiki2graph/crawler.py
@@ -31,14 +31,16 @@ class DefaultPrioritizationScheme:
 
 
 class Crawler:
-    def __init__(self, base_url, cache="cache/", prioritization_scheme=DefaultPrioritizationScheme):
+    def __init__(self, base_url, cache="cache/", prioritization_scheme=DefaultPrioritizationScheme, limit=100):
         self.base_url = base_url
+        self.limit = limit
 
         # create the cache_dir if not there
         self.cache_dir = cache
         if not os.path.exists(self.cache_dir):
             os.makedirs(self.cache_dir)
 
+        # load the cache dictionary
         try:
             with open(os.path.join(self.cache_dir, 'cache_dict.json')) as f:
                 self.cache_dict = json.load(f)
@@ -46,7 +48,7 @@ class Crawler:
             logger.warning("No cache found, creating...")
             with open(os.path.join(self.cache_dir, 'cache_dict.json'), 'w') as f:
                 self.cache_dict = {}
-                json.dump(self.cache_dict, f)
+                json.dump(self.cache_dict, f, indent=4)
 
         self.visited = []
 
@@ -63,18 +65,21 @@ class Crawler:
         return self
 
     def next(self):
-        if not self.next_pages:
-            raise StopIteration
+        if not self.next_pages or len(self.visited) == self.limit:
+            return None
 
         self.current_page = heappop(self.next_pages)[1]
+        while self.current_page in self.visited:
+            self.current_page = heappop(self.next_pages)[1]
 
+        # get the next page, either from the cache or the web
         if self.current_page in self.cache_dict:
             logger.info("Cache hit on {}".format(self.current_page))
             with open(os.path.join(self.cache_dir, self.cache_dict[self.current_page])) as f:
                 self.html = '\n'.join(f.readlines())
         else:
             logger.info('Getting page: {}'.format(self.current_page))
-            page_to_get = self.current_page
+            page_to_get = self.current_page  # for handling relative URLs
             if self.current_page.startswith('/'):
                 page_to_get = self.base_url + self.current_page
             self.html = requests.get(page_to_get).text
@@ -83,6 +88,7 @@ class Crawler:
         self.soup = BeautifulSoup(self.html, 'html.parser')
         self.visited.append(self.current_page)
         self.get_next_pages()
+        return self
 
     def get_next_pages(self):
         for link in self.soup.find_all('a'):
@@ -99,10 +105,9 @@ class Crawler:
         self.cache_dict[self.current_page] = file_id
         # save cache_dict
         with open(os.path.join(self.cache_dir, 'cache_dict.json'), 'w') as f:
-            json.dump(self.cache_dict, f)
+            json.dump(self.cache_dict, f, indent=4)
 
         # save html
-        print(os.getcwd())
         with open(os.path.join(self.cache_dir, file_id), 'w') as f:
             f.write(self.html)
 
@@ -111,7 +116,7 @@ class Crawler:
 
 
 if __name__ == '__main__':
-    c = Crawler('http://pokemon.wikia.com/wiki/Pokémon_Wiki')
+    c = Crawler('http://pokemon.wikia.com/wiki/Pokémon_Wiki', limit=200)
 
-    c.next()
-    print(c.current_title())
+    while c.next():
+        print(c.current_title())
